da82bb7771a6e357d175f4d40e96bf76e9276097
jahnvijoshi14 <jahnvijoshi312@gmail.com>
2023-05-27 20:00:39 +0530

    chasnge

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import AddTask from "./addtask";
